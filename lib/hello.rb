475e73d31d9630aff0b8e279252ce6fe97ac5f5d
Yurecarneiro <yure.carneiro2011@hotmail.com>
2015-09-10 20:42:03 -0300

    Added uses class

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,7 @@
-# Default is "World"
-# Author: Yurecarneiro (yure.carneiro2011@hotmail.com)
+require 'greeter'
+
+# Default is World
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
